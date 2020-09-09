3f82e7e5597f9c98c8404740fd08dea477efa0b0
Nolan Prochnau <parvus.mortalis@gmail.com>
2020-09-08 23:00:59 -0400

    Remove command utility in parmort.util.nvim

--- nvim/.config/nvim/lua/parmort/util/nvim.lua
+++ nvim/.config/nvim/lua/parmort/util/nvim.lua
@@ -1,12 +1,12 @@
 local M = {}
 local nvim = M
 
-M.command = vim.api.nvim_command
+-- M.command = vim.api.nvim_command
 
 function M.err(msg)
   M.command('echohl WarningMsg')
   M.command('echo "'..msg..'"')
-  M.command('echohl None')
+  vim.api.nvim_command('echohl None')
 end
 
 return nvim
