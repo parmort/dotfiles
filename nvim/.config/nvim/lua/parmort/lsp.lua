e509664f6d5abed16a417b5b33abe079736eb651
Nolan Prochnau <parvus.mortalis@gmail.com>
2020-09-08 22:43:36 -0400

    Fix my stupidity
    'signcolumn' is a window-local function, not a buffer-local.

--- nvim/.config/nvim/lua/parmort/lsp.lua
+++ nvim/.config/nvim/lua/parmort/lsp.lua
@@ -1,9 +1,10 @@
-local nvim = require'parmort.util'.nvim
 local nvim_lsp = require'nvim_lsp'
 local nlua_lsp = require'nlua.lsp.nvim'
 
 vim.fn.sign_define('LspDiagnosticsErrorSign', { text = 'E»' })
 vim.fn.sign_define('LspDiagnosticsWarningSign', { text = 'W»' })
+vim.fn.sign_define('LspDiagnosticsHintSign', { text = 'H»' })
+vim.fn.sign_define('LspDiagnosticsInformationSign', { text = 'I»' })
 
 local function mapluafn(mode, key, cmd)
   local value = '<cmd>lua '..cmd..'<CR>'
@@ -12,23 +13,21 @@ end
 
 local function configureBuffer()
   mapluafn('n', '<leader>r', 'vim.lsp.buf.rename()')
-  mapluafn('n', '<leader>e', 'vim.lsp.util.show_line_diagnostics()') -- Show PUM with errors
+  mapluafn('n', '<leader>e', 'vim.lsp.util.show_line_diagnostics()')
   mapluafn('n', 'gd', 'vim.lsp.buf.definition()')
-  mapluafn('n', 'K', 'vim.lsp.buf.hover()') -- Show documentation, etc. in PUM
+  mapluafn('n', 'K', 'vim.lsp.buf.hover()')
 
-  mapluafn('i', '<C-s>', 'vim.lsp.buf.signature_help()') -- Show function signature in PUM
+  mapluafn('i', '<C-s>', 'vim.lsp.buf.signature_help()')
 
-  nvim.command('setlocal signcolumn=yes')
+  vim.api.nvim_win_set_option(0, 'signcolumn', 'yes')
 end
 
 local common = {
   on_attach = configureBuffer,
 }
 
-nvim_lsp.tsserver.setup(common)
-
-nvim_lsp.vimls.setup(common)
-
-nvim_lsp.ccls.setup(common)
-
 nlua_lsp.setup(nvim_lsp, common)
+
+nvim_lsp.tsserver.setup(common)
+nvim_lsp.vimls.setup(common)
+nvim_lsp.ccls.setup(common)
