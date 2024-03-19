7ac7d0b7c2a52f44493a8cdda64d5b64b47cb2f0
Nolan Prochnau <parvus.mortalis@gmail.com>
2024-03-19 14:24:51 -0400

    (nvim) Add more obsidian configs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -80,5 +80,30 @@ require('obsidian').setup {
       name = "vault",
       path = "~/documents/vault"
     }
-  }
+  },
+
+  note_path_func = function(spec)
+    local path = spec.dir / spec.title
+    return path:with_suffix('.md')
+  end,
+
+  ---@return table
+  note_frontmatter_func = function(note)
+    -- Add the title of the note as an alias.
+    if note.title then
+      note:add_alias(note.title)
+    end
+
+    local out = { aliases = note.aliases, tags = note.tags }
+
+    -- `note.metadata` contains any manually added fields in the frontmatter.
+    -- So here we just make sure those fields are kept in the frontmatter.
+    if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+      for k, v in pairs(note.metadata) do
+        out[k] = v
+      end
+    end
+
+    return out
+  end,
 }
