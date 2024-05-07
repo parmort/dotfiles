fb59664601097e90bd721fe17b6172cfac3d5bf8
Nolan Prochnau <parvus.mortalis@gmail.com>
2024-05-07 13:26:05 -0400

    Lump commit

--- nvim/.config/nvim/lua/parmort/util.lua
+++ nvim/.config/nvim/lua/parmort/util.lua
@@ -1,8 +1,8 @@
-local M = {}
+local util = {}
 
 --- @param group string
---- @param attr table|string If `attr` is a string, then `group` is linked to `attr`; otherwise, `attr` is a table of attributes for `:highlight`
-function M.highlight(group, attr)
+--- @param attr table|string If a string, then `group` is linked to `attr`; otherwise, `attr` is a table of attributes
+function util.highlight(group, attr)
   if type(attr) == 'string' then
     vim.cmd(string.format('highlight link %s %s', group, attr))
     return
@@ -20,7 +20,7 @@ end
 --- @param fg   string|nil
 --- @param bg   string|nil
 --- @param attr string|nil
-function M.gui_attrs(fg, bg, attr)
+function util.gui_attrs(fg, bg, attr)
   return {
     guifg = fg or 'fg',
     guibg = bg or 'bg',
@@ -32,7 +32,7 @@ end
 --- @param lhs string
 --- @param rhs string
 --- @param options? {remap:string}
-function M.abbrev(mode, lhs, rhs, options)
+function util.abbrev(mode, lhs, rhs, options)
   options = options or {}
   local command = {
     cmd = mode,
@@ -48,4 +48,4 @@ function M.abbrev(mode, lhs, rhs, options)
   vim.cmd(command)
 end
 
-return M
+return util
--- nvim/.config/nvim/plugin/telescope.lua
+++ nvim/.config/nvim/plugin/telescope.lua
@@ -1,12 +1,38 @@
 local leader = require('parmort.keymap').leader
 local cmd = require('parmort.keymap').command
 
-local function keymap(mode, lhs, rhs, opts)
-  local action = string.format(cmd 'lua require"parmort.telescope".%s()', rhs)
+local function map_builtin(mode, lhs, rhs, opts)
+  local action = string.format(cmd 'lua require"telescope.builtin".%s()', rhs)
   vim.keymap.set(mode, lhs, action, opts)
 end
 
-keymap('n', leader 't', 'git_files')
-keymap('n', leader 'h', 'help_tags')
-keymap('n', leader 'p', 'reloader')
-keymap('n', leader 'c', 'commands')
+require('telescope').setup({
+  defaults = { -- Dropdown theme, without the previewer
+    results_title = false,
+    previewer = false,
+    sorting_strategy = "ascending",
+    layout_strategy = "center",
+
+    layout_config = {
+      width = function(_, max_cols, _)
+        return math.min(max_cols, 80)
+      end,
+
+      height = function(_, _, max_lines)
+        return math.min(max_lines, 15)
+      end
+    },
+
+    border = true,
+    borderchars = {
+      prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
+      results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
+      preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+    },
+  }
+})
+
+map_builtin('n', leader 't', 'git_files')
+map_builtin('n', leader 'h', 'help_tags')
+map_builtin('n', leader 'p', 'reloader')
+map_builtin('n', leader 'c', 'commands')
